2476e628718ff573260723e0fc559404bf70c0df
KhanhToan3010 <khanhtoan18079261@gmail.com>
2021-09-24 17:14:47 +0700

    Xong bài hoa

---
+++ app/src/main/java/com/example/animation_flower_1/bai1/DetailHoa2.java
@@ -0,0 +1,16 @@
+package com.example.animation_flower_1.bai1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.R;
+
+public class DetailHoa2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_hoa2);
+    }
+}
